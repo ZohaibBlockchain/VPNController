2dfe0a5ee065c080707ed2fabfd742df6aa2c32f
ZohaibBlockchain <113122909+ZohaibBlockchain@users.noreply.github.com>
2024-06-04 15:55:53 +0400

    fix

--- controller.js
+++ controller.js
@@ -54,12 +54,17 @@ app.listen(PORT, () => {
 
 
 
+
+
+
 const Limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10, // limit each IP to 10 requests per windowMs
-    message: 'Too many attempts from this IP, please try again after 15 minutes.'
-});
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  });
 
+  app.use(Limiter);
 
 
 // Routes
@@ -198,7 +203,7 @@ app.post('/api/selectedserver', async (req, res) => {
 
 
 
-app.post('/api/deactivate', Limiter, async (req, res) => {
+app.post('/api/deactivate', async (req, res) => {
 
     // Check if the authorization header is present and has the correct format
     if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
